a79d857ad7e1d536af09cafda4ef31a30c8aa0ba
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-08-12 12:59:39 +0900

    Added getById api route and function to get specific element and not the whole list.

--- index.js
+++ index.js
@@ -26,6 +26,35 @@ app.get('/', (req, res, next) => {
 	);
 });
 
+// GET /id Route
+app.get('/:id', (req, res, next) => {
+	repo.getById(req.params.id, function (data) {
+		if (data) {
+			// Code to execute when call is successful
+			res.send({
+				"status": 200,
+				"statusText": "OK",
+				"message": "Single product retrieved.",
+				"data": data
+			});
+		}
+		else {
+			let msg = `The product '${req.params.id}' could not be found.`;
+			res.status(404).send({
+				"status": 404,
+				"statusText": "Not Found",
+				"message": msg,
+				"error": {
+					"code": "NOT_FOUND",
+					"message": msg
+				}
+			});
+		}
+	}, function(err) {
+		next(err);
+	});
+});
+
 // Create web server to listen on the specified port
 let server = app.listen(port, function () {
 	console.log(`NodeAPI server is running on http://localhost:${port}.`);
