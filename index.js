2fe98b1573f6fec6eb1954cf3670da1372661011
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-08-14 14:36:38 +0900

    Default Express error handling overwritten

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ router.use('/product', require('./routes/product'));
 
 app.use(prefix, router);
 
+// Force Express to send error 500 request back to caller if error occurs instead of html as default
+app.use(function (err, req ,res ,next) {
+    res.status(500).send(err);
+});
+
 let server = app.listen(port, function () {
 	console.log(`NodeAPI server is running on ${host}:${port}.`);
 });
