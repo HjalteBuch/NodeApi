885cd96e0d59269f1b4bdcfbcff015dcad224520
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-08-14 14:21:10 +0900

    Added config npm package. Added config file for development environment. Replaced hardcoded values in code with values form the new config file.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 app.use(express.json());
-const port = 3000;
+
+const config = require('config');
+const host = config.get('host');
+const prefix = config.get('prefix');
+const port = config.get('port');
 
 router.use('/product', require('./routes/product'));
 
-app.use('/api', router);
+app.use(prefix, router);
 
 let server = app.listen(port, function () {
-	console.log(`NodeAPI server is running on http://localhost:${port}.`);
+	console.log(`NodeAPI server is running on ${host}:${port}.`);
 });
