88ee2469418006464ede443987e7c2f9774dd3a2
Hjalte Hesbjerg <hjalte@Hjaltes-MBP.lan>
2023-08-15 18:32:34 +0900

    Small cleanup in index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-app.use(express.json());
 
 process.env["NODE_ENV"] = "development";
 const config = require('config');
@@ -11,6 +10,7 @@ const port = config.get('port');
 
 router.use('/product', require('./routes/product'));
 
+app.use(express.json());
 app.use(prefix, router);
 
 // Force Express to send error 500 request back to caller if error occurs instead of html as default
