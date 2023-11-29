94b815985661dbb0773a30e284bf1be499dcafdc
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-11-29 16:31:22 +0900

    Added an sql helper to repeat less code when setting up connection to sqlite db. Added connectionString to config

---
+++ helpers/sqlserver.js
@@ -0,0 +1,26 @@
+let sqlite = require('sqlite3');
+
+const config = require('config');
+
+let connectString = config.get('connectString');
+
+let db = {};
+
+db.submit = async function (sql, params, resolve, reject) {
+    try {
+        let db = new sqlite.Database('/Users/hjalte/Projects/NodeApi/db/AdvWorksProducts.db');
+
+        db.all(sql, params, (err, rows ) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(rows);
+        });
+
+        db.close();
+    } catch (err) {
+        reject(err);
+    }
+}
+
+module.exports = db
--- repositories/product-sqlServer.js
+++ repositories/product-sqlServer.js
@@ -1,56 +1,18 @@
-const sqlite3 = require('sqlite3').verbose();
+let db = require('../helpers/sqlserver');
 
-let repo = {};
+let repo = {
+    get: function (resolve, reject) {
+        let sql = `
+        SELECT productID, name, productNumber, color, standardCost, listPrice, modifiedDate
+        FROM Product
+        Order BY name, listPrice`;
 
-repo.get = async function (resolve, reject) {
-    let db = new sqlite3.Database('/Users/hjalte/Projects/NodeApi/db/AdvWorksProducts.db');
-    // let db = new sqlite3.Database('/Users/hjalte/Projects/NodeApi/db/AdvWorksProducts.db', sqlite3.OPEN_READWRITE,(err) => {
-    //   if (err) {
-    //     return console.error(err.message);
-    //   }
-    //   console.log('Connected to AdvWorksProducts SQlite database.');
-    // });
-
-    let sql = `
-    SELECT productID, name, productNumber, color, standardCost, listPrice, modifiedDate
-    FROM Product
-    Order BY name, listPrice`;
-
-    let result = {};
-    db.all(sql,[],(err, rows ) => {
-        if (err) {
+        db.submit(sql, null, function (data) {
+            resolve(data);
+        }, function (err) {
             reject(err);
-        }
-        result = rows;
-        resolve(result);
-    });
-
-    // db.all(sql, [], (err, rows) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     rows.forEach((row) => {
-    //         console.log(row.name);
-    //     });
-    // });
-
-    db.close()
-    // db.close((err) => {
-    //   if (err) {
-    //     return console.error(err.message);
-    //   }
-    //   console.log('Close the database connection.');
-    // });
-}
-
-repo.get(
-    function(data) {
-        console.log(typeof(data));
-        console.log(Object.keys(data));
-    },
-    function(err) {
-        console.error(err);
+        });
     }
-);
+}
 
 module.exports = repo;
