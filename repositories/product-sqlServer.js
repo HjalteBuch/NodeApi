4de9cc0c61ba6c9e01094a2065a69ea626643a21
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-11-30 00:01:39 +0900

    Added search functionallity withthe sql database

--- repositories/product-sqlServer.js
+++ repositories/product-sqlServer.js
@@ -34,4 +34,31 @@ repo.getById = function (id, resolve, reject) {
     });
 }
 
+repo.search = function (search, resolve, reject) {
+    // 1=1 because I have to have a whereStatement, and 1=1 will always be true
+    // Kind of janky, but I like it
+    let sql = 'SELECT * FROM Product WHERE 1=1';
+
+    let params = [];
+
+    if (search.name){
+        sql += ` AND name LIKE "${search.name}%"`;
+        params.push(search.name);
+    }
+    if (search.listPrice){
+        sql += ` AND listPrice >= ${search.listPrice}`;
+        params.push(search.listPrice);
+    }
+
+    db.submit(sql, [], function (data) {
+        if (data.length){
+            resolve(data);
+        } else {
+            resolve(undefined);
+        }
+    }, function (err) {
+        reject(err);
+    });
+}
+
 module.exports = repo;
